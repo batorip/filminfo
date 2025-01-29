d0422790744a7de23f9a12ee8984eaca72648ef9
Batori Peter <batori.peter@gmail.com>
2025-01-29 15:15:33 +0100

    HTML file generation and formatting completed. Database cleaning and deletion of unnecessary image files completed. Translation prepared.

--- filminfo.py
+++ filminfo.py
@@ -4,22 +4,13 @@ import tmdbsimple as tmdb
 import sqlite3
 import datetime
 import json
-
-# Setting up the API key
-with open('filminfo.json', 'r') as f:
-  data = json.load(f)
-tmdb.API_KEY = data['API_KEY']['tmdb_API_key']
-
-# Setting the folder containing videos
-directory = data['DIRECTORY']['videos']
-
-# Setting the language
-lang = data['LANGUAGE']['lang']
+import locale
+import filminfo_html
 
 # Searches for and returns the title in the name of the item
 def clean_title(item_1st_part, is_tv):
     if is_tv:
-        match = re.search(r'(.*?)(S\d{2}(-S\d{2})?|Complete|COMPLETE|Season)', item_1st_part, re.IGNORECASE)
+        match = re.search(r'(.*?)(S\d{2}(-S\d{2})?|Complete|Season)', item_1st_part, re.IGNORECASE)
         if match:
             return re.sub(r'\.', ' ', match.group(1)).strip()
         return re.sub(r'\.', ' ', item_1st_part).strip()  # If there is no season designation, the entire name
@@ -30,18 +21,22 @@ def clean_title(item_1st_part, is_tv):
         return re.sub(r'\.', ' ', item_1st_part).strip()  # If there is no year, the whole name
 
 # Determines the title of the item and the year (if any)
-def extract_year_and_title(item, type):
+def extract_title_and_year(item, directory, film_type):
+    # If the item is a file, delete its extension.
+    if os.path.isfile(os.path.join(directory, item)):
+        item = item.rsplit(".", 1)[0]
+
+    # Search for title and year in the item.
     match = re.search(r'(.+?)(\d{4})', item)
     if match:
-        title = clean_title(match.group(1), type=="tv")
+        title = clean_title(match.group(1), film_type == "tv")
         year = match.group(2)
         return title, year
     # If there is no year, just return the cleaned title
-    return clean_title(item, type == "tv"), None
+    return clean_title(item, film_type == "tv"), None
 
 # Determines whether the item is a TV series.
 def get_film_type(item):
-    #return re.search(r'(?<!\w)(S\d{2}(-S\d{2})?|Season|Complete)(?!\d|\w)', item, re.IGNORECASE) is not None
     return 'tv' if re.search(r'(?<!\w)(S\d{2}(-S\d{2})?|Season|Complete)(?!\d|\w)', item, re.IGNORECASE) is not None else 'movie'
 
 # Performs an SQL query and returns the result in a dictionary.
@@ -58,8 +53,9 @@ def query_as_dict(cursor, query, params=None):
         return None
 
 # Based on the title and year of the item, it queries the movie data from the tmdb.
-def get_movie_details(search_title, search_year):
+def get_movie_details(db, directory, item, lang):
     cursor =  db.cursor()
+    search_title, search_year = extract_title_and_year(item, directory,'movie')
     if search_year:
         values = query_as_dict(cursor, "SELECT * FROM movie WHERE search_title = ? AND search_year=?", (search_title, search_year))
     else:
@@ -68,7 +64,8 @@ def get_movie_details(search_title, search_year):
     if values:
         del values['id']
     else:
-        values = {"search_title": search_title,
+        values = {"item": item,
+                  "search_title": search_title,
                   "search_year": search_year,
                   "query_date": datetime.datetime.now().strftime("%x")}
 
@@ -101,26 +98,26 @@ def get_movie_details(search_title, search_year):
                 else:
                     values[f'cast_{i + 1}'] = ''
 
-            sql = '''INSERT INTO movie(search_title, search_year, movie_id, query_date, title, original_title, overview, release_date, runtime, vote_average, genres, poster_path, cast_1, cast_2, cast_3, cast_4, cast_5)
-                     VALUES(:search_title, :search_year, :movie_id, :query_date, :title, :original_title, :overview, :release_date, :runtime, :vote_average, :genres, :poster_path, :cast_1, :cast_2, :cast_3, :cast_4, :cast_5)'''
+            sql = '''INSERT INTO movie(item, search_title, search_year, movie_id, query_date, title, original_title, overview, release_date, runtime, vote_average, genres, poster_path, cast_1, cast_2, cast_3, cast_4, cast_5)
+                     VALUES(:item, :search_title, :search_year, :movie_id, :query_date, :title, :original_title, :overview, :release_date, :runtime, :vote_average, :genres, :poster_path, :cast_1, :cast_2, :cast_3, :cast_4, :cast_5)'''
         else:
-            sql = '''INSERT INTO movie(search_title, search_year, query_date)
-                     VALUES(:search_title, :search_year, :query_date)'''
-        print(values)
+            sql = '''INSERT INTO movie(item, search_title, search_year, query_date)
+                     VALUES(:item, :search_title, :search_year, :query_date)'''
         cursor.execute(sql, values)
         db.commit()
     return values
 
 # Based on the title of the item, it queries the TV series data from the tmdb
-def get_tv_details(search_title):
+def get_tv_details(db, directory, item, lang):
     cursor = db.cursor()
-
+    search_title, _ = extract_title_and_year(item, directory, 'tv')
     values = query_as_dict(cursor, "SELECT * FROM tv WHERE search_title = ?", (search_title,))
 
     if values:
         del values['id']
     else:
-        values = {"search_title": search_title,
+        values = {"item": item,
+                  "search_title": search_title,
                   "query_date": datetime.datetime.now().strftime("%x")}
 
         details = None
@@ -152,24 +149,24 @@ def get_tv_details(search_title):
                 else:
                     values[f'cast_{i + 1}'] = ''
 
-            sql = '''INSERT INTO tv(search_title, tv_id, query_date, name, original_name, overview, first_air_date, last_air_date, number_of_episodes, number_of_seasons, vote_average, genres, poster_path, cast_1, cast_2, cast_3, cast_4, cast_5)
-                     VALUES(:search_title, :tv_id, :query_date, :name, :original_name, :overview, :first_air_date, :last_air_date, :number_of_episodes, :number_of_seasons, :vote_average, :genres, :poster_path, :cast_1, :cast_2, :cast_3, :cast_4, :cast_5)'''
+            sql = '''INSERT INTO tv(item, search_title, tv_id, query_date, name, original_name, overview, first_air_date, last_air_date, number_of_episodes, number_of_seasons, vote_average, genres, poster_path, cast_1, cast_2, cast_3, cast_4, cast_5)
+                     VALUES(:item, :search_title, :tv_id, :query_date, :name, :original_name, :overview, :first_air_date, :last_air_date, :number_of_episodes, :number_of_seasons, :vote_average, :genres, :poster_path, :cast_1, :cast_2, :cast_3, :cast_4, :cast_5)'''
         else:
-            sql = '''INSERT INTO tv(search_title, query_date)
-                     VALUES(:search_title, :query_date)'''
+            sql = '''INSERT INTO tv(item, search_title, query_date)
+                     VALUES(:item, :search_title, :query_date)'''
         cursor.execute(sql, values)
         db.commit()
     return values
 
 # Create the sqlite3 database if it does not already exist.
 def create_sqlite_db():
-    global db
     db = sqlite3.connect('filminfo.db')
 
     db.execute(  '''CREATE TABLE IF NOT EXISTS movie (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    item TEXT NOT NULL,
                     search_title TEXT NOT NULL,
-                    search_year INTEGER,
+                    search_year TEXT,
                     query_date TEXT NOT NULL,
                     movie_id INTEGER,
                     title TEXT,
@@ -186,8 +183,10 @@ def create_sqlite_db():
                     cast_4 TEXT,
                     cast_5 TEXT )''')
 
+
     db.execute(  '''CREATE TABLE IF NOT EXISTS tv (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    item TEXT NOT NULL,
                     search_title TEXT NOT NULL,
                     query_date TEXT NOT NULL,
                     tv_id INTEGER,
@@ -206,30 +205,84 @@ def create_sqlite_db():
                     cast_3 TEXT,
                     cast_4 TEXT,
                     cast_5 TEXT )''')
+    return db
+
+# A comparison function for the sorted() function.
+def sort_by_key(film, key, loc):
+    try:
+        locale.setlocale(locale.LC_ALL, loc)
+    except Exception as e:
+        print(f"Incorrect locale setting ({loc}): {e}")
+        print("Please check the filminfo.json file and try again.")
+        exit(1)
+    return locale.strxfrm(film[key].lower())
+
+# Delete unnecessary database rows.
+def delete_unnecessary_db_rows(db, table_name, films):
+    cursor = db.cursor()
+    films = [s.replace("'", "''") for s in films]
+    cmd = "DELETE FROM " + table_name + " WHERE item NOT IN ({})".format(', '.join("'" + str(kulcs) + "'" for kulcs in films))
+    cursor.execute(cmd)
+    db.commit()
 
 # The main program
 def main():
-    global db
+    with open('filminfo.json', 'r') as f:
+        data = json.load(f)
+        f.close()
 
-    create_sqlite_db()
-    for file_or_dir in os.listdir(directory):
-        #if os.path.isdir(os.path.join(directory, item)):
-        film_type = get_film_type(file_or_dir)
+    # Setting up the API key
+    tmdb.API_KEY = data['tmdb_API_key']
 
-        title, year = extract_year_and_title(file_or_dir, film_type)
+    # Setting the folder containing videos
+    directory = data['film_dir']
 
-        print(f"Item: {file_or_dir}")
-        print(f"Type: {film_type}")
+    # Setting the language
+    lang = data['lang']
+
+    # Setting the locale
+    loc = data['locale']
+
+    db = create_sqlite_db()
+    # Separate TV, movie and unidentified films:
+    tv_films = []
+    movie_films = []
+    unknown_items = []
+
+    # The films that still exist, have not been deleted:
+    existing_movie_films = []
+    existing_tv_films = []
+
+    for item in os.listdir(directory):
+        film_type = get_film_type(item)
 
         if film_type == 'tv':
-            result = get_tv_details(title)
+            result = get_tv_details(db, directory, item, lang)
+            result["item"] = item
+            existing_tv_films.append(item)
+            if result.get("tv_id") is not None:
+                tv_films.append(result)
+            else:
+                unknown_items.append(result)
         else:
-            result = get_movie_details(title, year)
-        for key, value in result.items():
-            print(f"{key}: {value}")
-        print()
+            result = get_movie_details(db, directory, item, lang)
+            result["item"] = item
+            existing_movie_films.append(item)
+            if result.get("movie_id") is not None:
+                movie_films.append(result)
+            else:
+                unknown_items.append(result)
 
+    movie_films = sorted(movie_films, key=lambda film: sort_by_key(film, 'title', loc))
+    tv_films = sorted(tv_films, key=lambda film: sort_by_key(film, 'name', loc))
+    unknown_items = sorted(unknown_items, key=lambda film: sort_by_key(film, 'item', loc))
+
+    delete_unnecessary_db_rows(db, 'movie', existing_movie_films)
+    delete_unnecessary_db_rows(db, 'tv', existing_tv_films)
     db.close()
 
+    filminfo_html.create_tables(movie_films, tv_films, unknown_items)
+
+# Program entry point
 if __name__ == "__main__":
   main()
---
+++ filminfo_html.py
@@ -0,0 +1,309 @@
+import os
+import shutil
+
+import requests
+from urllib.parse import urljoin
+import json
+# from gettext import gettext as _
+import gettext
+
+
+with open('filminfo.json', 'r') as f:
+    data = json.load(f)
+    f.close()
+# Setting the language
+language = data['lang']
+url_path = data['url_path']
+html_dir=data["html_dir"]
+
+# translate text
+lang = gettext.translation('messages', localedir='locale', languages=['hu'])
+lang.install()
+_ = lang.gettext
+
+# Downloads the image or loads it from the cache.
+def download_image(poster_path, image_dir):
+    # image sizes: w95, w154, w185, w342, w500, ...
+    # url: https://image.tmdb.org/t/p/<size>/<filename>
+
+    url = urljoin("https://image.tmdb.org/t/p/w342/", poster_path.lstrip("/"))
+    file_path = os.path.join(image_dir.rstrip("/"), os.path.basename(poster_path))
+
+    # If the image is not in cache, download and save it.
+    if not os.path.exists(file_path):
+        response = requests.get(url, stream=True)
+        if response.status_code == 200:  # Sikeres válasz
+            with open(file_path, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=8192):  # Részenként írja a fájlt
+                    f.write(chunk)
+                f.close()
+            print(poster_path+' '+_('image downloaded successfully.'))
+        else:
+            print(poster_path + ' ' + _('image download failed.'))
+
+def create_html_files(table_name, table_data, table_names_and_texts):
+    image_dir = html_dir.rstrip("/") + "/images"
+
+    # Create html folder if it doesn't exist
+    if not os.path.exists(html_dir):
+        os.makedirs(html_dir)
+
+    # Create image folder if it doesn't exist
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+
+    tmdb_svg_path = os.path.join(image_dir, "tmdb.svg")
+    if not os.path.isfile(tmdb_svg_path):
+        shutil.copy("tmdb.svg", tmdb_svg_path)
+
+    style_css_path = os.path.join(html_dir, "style.css")
+    if not os.path.exists(style_css_path):
+        os.remove(style_css_path)
+    shutil.copy("style.css", style_css_path)
+
+    li_items =[]
+    for key, value in table_names_and_texts.items():
+        if key == table_name:
+            li_items.append(f'<li><A CLASS="active" HREF="{key}.html">{value}</A></li>')
+        else:
+            li_items.append(f'<li><A HREF="{key}.html">{value}</A></li>')
+
+    html_begin = [
+        '<!DOCTYPE html>',
+        f'<html lang="{language}">',
+        '<head>',
+        '<meta charset="utf-8">',
+        '<title>filminfo</title>',
+        '<link rel="stylesheet" href="style.css" type="text/css">',
+        '<link rel="stylesheet" href="custom.css" type="text/css">',
+        '</head>',
+        '<body>',
+        '<header>',
+        '<h1>filminfo</h1>',
+        '</header>',
+        '<nav>',
+        '<ul class="menu">' ]
+    html_begin += li_items
+    html_begin += [
+        '</ul>',
+        '</nav>',
+        '<article>',
+        '<table>' ]
+    html_end = [
+        '</table>',
+        '</article>',
+        '<footer>',
+        '<A HREF="https://www.themoviedb.org/" TARGET="_blank"><IMG SRC="images/tmdb.svg" WIDTH="80"></A>&nbsp;',
+        'This program uses TMDB and the TMDB APIs but is not endorsed, certified, or otherwise approved by TMDB.',
+        '</footer>',
+        '</body>',
+        '</html>' ]
+    html_file = open(html_dir +"/" + table_name + ".html", "w")
+    for line in html_begin:
+        html_file.write(line+'\n')
+
+    if "grid" in table_data:
+        html_file.write('<colgroup>\n')
+        for i in range(1, len(table_data["grid"][0]) + 1):
+            html_file.write(f'<col class="col_{i}">\n')
+        html_file.write('</colgroup>\n')
+
+        for film in table_data["film"]:
+            for grid_row, grid_line in enumerate(table_data["grid"]):
+                html_file.write(f'<tr class="tr_{grid_row+1}">\n')
+                for grid_col, grid_cell in enumerate(grid_line):
+                    # If this is the first row, or it is NOT part of the cell above it (not rowspan):
+                    if grid_row == 0 or grid_cell != table_data["grid"][grid_row - 1][grid_col]:
+                        if isinstance(grid_cell, tuple):
+                            cell_value = table_data["separator"][grid_row][grid_col].join([str(film[key]) for key in grid_cell if key in film and film[key]])
+                        elif grid_cell is not None:
+                            cell_value = film[grid_cell]
+                        else:
+                            cell_value = _("ERROR")
+
+                        row_span = 1
+                        while grid_row + row_span < len(table_data["grid"]) and grid_cell == \
+                                table_data["grid"][grid_row + row_span][grid_col]:
+                            row_span += 1
+                        td_class = table_data["class"][grid_row][grid_col]
+                        if td_class:
+                            td_params = f' class="td_{td_class}"'
+                        else:
+                            td_params = ''
+
+                        if row_span>1:
+                            td_params += f' rowspan = "{row_span}"'
+
+                        html_file.write(f'<td{td_params}>\n')
+
+                        cell_title = table_data["cell_title"][grid_row][grid_col]
+                        cell_value_suffix = table_data["suffix"][grid_row][grid_col]
+                        cell_value_suffix = cell_value_suffix if cell_value_suffix is not None else ''
+
+                        cell_specialty = table_data["specialty"][grid_row][grid_col]
+                        if cell_specialty == "IMG":
+                            download_image(cell_value, image_dir)
+                            html_file.write(f'<img src="images/{cell_value.lstrip("/")}" class="{table_data["class"][grid_row][grid_col]}_img">')
+                        else:
+                            if cell_title:
+                                html_file.write(f'<div class="cell_title">{cell_title}:</div>\n')
+                            cell_value_class = table_data["class"][grid_row][grid_col]
+                            start_of_anchor = f'<a href="{url_path.rstrip("/")}/{film["item"].lstrip("/")}" class="film_link">' if cell_specialty == "URL" else ''
+                            end_of_anchor = '</A>' if cell_specialty == "URL" else ''
+                            html_file.write(f'<div class="{cell_value_class}">'
+                                            f'{start_of_anchor}{str(cell_value)} {str(cell_value_suffix)}{end_of_anchor}</div>\n')
+                        html_file.write('</td>\n')
+                html_file.write('</tr>\n')
+            html_file.write(f'<tr><td colspan="{len(table_data["grid"][0])}" class="empty"></td></tr>\n')
+
+
+    else:
+        for film in table_data["film"]:
+            html_file.write(f'<tr><td>{film["item"]}</td></tr>')
+    for line in html_end:
+        html_file.write(line + '\n')
+    html_file.close()
+
+def create_tables(movie_films, tv_films, unknown_items):
+    cast = tuple(f"cast_{i}" for i in range(1, 6))
+    air_dates = ('first_air_date', 'last_air_date')
+    seasons_episodes = ('number_of_seasons', 'number_of_episodes')
+    table_data = {
+        "Movie": {"film": movie_films,
+                  "button_text": _("Movie"),
+                  "grid": (
+                      ('poster_path', 'title',          'overview', cast),
+                      ('poster_path', 'original_title', 'overview', cast),
+                      ('poster_path', 'genres',         'overview', cast),
+                      ('poster_path', 'vote_average',   'overview', cast),
+                      ('poster_path', 'release_date',   'overview', cast),
+                      ('poster_path', 'runtime',        'overview', cast)
+                  ),
+                  "class": (
+                      ('poster',      'title',          'overview', 'cast'),
+                      (None,          'o_title',        None,       None),
+                      (None,          'genres',         None,       None),
+                      (None,          'vote',           None,       None),
+                      (None,          'date',           None,       None),
+                      (None,          'runtime',        None,       None)
+                  ),
+                  "separator": (
+                      (None,          None,             None,       '<br>'),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None)
+                  ),
+                  "cell_title": (
+                      (None,       _("Title"),      _("Overview"), _("Cast")),
+                      (None,       _("Original title"), None,       None),
+                      (None,       _("Genres"),         None,       None),
+                      (None,       _("Vote"),           None,       None),
+                      (None,       _("Release date"),   None,       None),
+                      (None,       _("Runtime"),        None,       None)
+                  ),
+                  "suffix": (
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          _("minutes"),     None,       None)
+                  ),
+
+                  "specialty": (
+                      ("IMG",         "URL",            None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None)
+                  )
+                  },
+
+        "TV": {"film": tv_films,
+               "button_text": _("TV"),
+
+               "grid": (
+                      ('poster_path', 'name',           'overview', cast),
+                      ('poster_path', 'original_name',  'overview', cast),
+                      ('poster_path', 'genres',         'overview', cast),
+                      ('poster_path', 'vote_average',   'overview', cast),
+                      ('poster_path',  air_dates,       'overview', cast),
+                      ('poster_path',  seasons_episodes,'overview', cast)
+                  ),
+                  "class": (
+                      ('poster',      'name',          'overview', 'cast'),
+                      (None,          'o_name',         None,       None),
+                      (None,          'genres',         None,       None),
+                      (None,          'vote',           None,       None),
+                      (None,          'date',           None,       None),
+                      (None,          'seasons_episodes',None,      None)
+                  ),
+                  "separator": (
+                      (None,          None,             None,       '<br>'),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          ' - ',            None,       None),
+                      (None,          '/',              None,       None)
+                  ),
+                  "cell_title": (
+                      (None,       _("Name"),      _("Overview"), _("Cast")),
+                      (None,       _("Original name"),  None,       None),
+                      (None,       _("Genres"),         None,       None),
+                      (None,       _("Vote"),           None,       None),
+                      (None,       _("Air dates"),      None,       None),
+                      (None,       _("Seasons/Episodes"),None,      None)
+                  ),
+                  "suffix": (
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None)
+                  ),
+
+                  "specialty": (
+                      ("IMG",         "URL",            None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None),
+                      (None,          None,             None,       None)
+                  )
+                  },
+
+        "Unknown": {"film": unknown_items,
+                    "button_text": _("Unknown")
+                    }
+    }
+
+    table_names_and_texts ={key:table_data[key]["button_text"] for key in table_data.keys()}
+
+    for name in table_data.keys():
+        create_html_files(name, table_data[name], table_names_and_texts)
+
+    # Delete unnecessary image files.
+    image_dir = html_dir.rstrip("/") + "/images"
+    all_files = set()
+    req_files = {'tmdb.svg'} # Required for the footer.
+    for filename in os.listdir(image_dir):
+        file_path = os.path.join(image_dir, filename)
+        if os.path.isfile(file_path):
+            all_files.add(filename)
+
+    for name in table_data.keys():
+        for film in table_data[name]["film"]:
+            if film["poster_path"]:
+                req_files.add(film["poster_path"].lstrip("/"))
+    to_delete = all_files - req_files
+    for filename in to_delete:
+        file_path = os.path.join(image_dir, filename)
+        os.remove(file_path)
+
+# Program entry point
+if __name__ == "__main__":
+  print("Do not run this script directly.\nThe filminfo.py script uses this.")
